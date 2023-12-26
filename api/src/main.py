ff6281d1f2cf4680b192a28c781431a787b95f45
tatsuyafukui <nakedtatsuya@gmail.com>
2023-12-26 16:53:05 +0900

    add backend

--- api/src/main.py
+++ api/src/main.py
@@ -5,14 +5,14 @@ import insightface
 
 face_analysis = insightface.app.FaceAnalysis()
 face_analysis.prepare(ctx_id=0, det_size=(640, 640))
-image1 = cv2.imread("./api/image.jpg")
+image1 = cv2.imread("./src/image.jpg")
 faces = face_analysis.get(image1)
 
 for face in faces:
 
     # 顔認識
     # match = compare_faces('./api/me.png', face_encoding, tolerance=0.4)
-    similarity = compute_sim(target_face=face, registed_face_image_path="./api/me.png")
+    similarity = compute_sim(target_face=face, registed_face_image_path="./src/me.png")
     if similarity < 0.6:
         # 登録済みの顔でない場合にモザイクを適用
         bbox = face.bbox.astype(int)
@@ -22,5 +22,5 @@ for face in faces:
         image = apply_mosaic(image1, x, y, width, height, mosaic_size=10)
 
 # 画像の保存
-cv2.imwrite('./api/mosaic_faces.jpg', image)
+cv2.imwrite('./src/mosaic_faces.jpg', image)
 
