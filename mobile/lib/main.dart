60c38a79d610fa20142e89759b15a8b929259df8
tatsuyafukui <nakedtatsuya@gmail.com>
2024-01-13 20:26:00 +0900

    add backend

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,11 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mosaic_app/providers/user.dart';
+import 'package:provider/provider.dart';
 import 'screens/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => UserProvider()),
+      // ChangeNotifierProvider(create: (context) => CartModel()),
+      // Provider(create: (context) => SomeOtherClass()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
